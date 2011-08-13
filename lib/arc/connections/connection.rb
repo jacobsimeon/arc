fb905af23e9ef72bfef120bcb910b8138ca1de91
Jacob Morris <jacob.s.morris@gmail.com>
2011-08-13 12:30:28 -0600

    define abstract methods for connection

--- lib/arc/connections/connection.rb
+++ lib/arc/connections/connection.rb
@@ -1,6 +1,24 @@
 module Arc
   module Connections
     class Connection
+      
+      def execute sql
+        not_implemented :execute
+      end
+      def active?
+        not_implemented :active?
+      end
+      def reconnect!
+        not_implemented :reconnect!
+      end
+      
+      private
+      def not_implemented method
+        raise NotImplementedError, <<-ERROR
+          "Arc::Connections::Connection does not implement ##{method}.  This method should be overwritten by derived classes"
+        ERROR
+      end 
+
     end
   end
 end
---
+++ spec/connections/connection_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+module Arc
+  module Connections
+    describe Connection do
+      
+      it 'specifies methods that should be overwritten by specific adapters' do
+        c = Connection.new
+        c.should respond_to(:execute)
+        c.should respond_to(:active?)
+        c.should respond_to(:reconnect!)
+        
+        lambda { c.execute "INSERT INTO superheros VALUES(superman)"}.should raise_error(NotImplementedError)
+        lambda { c.active? }.should raise_error(NotImplementedError)
+        lambda { c.reconnect! }.should raise_error(NotImplementedError)        
+      end
+            
+    end
+  end
+end
