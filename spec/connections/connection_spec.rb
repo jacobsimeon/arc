fd1632c90e58199aeb356eeaa118c0a9b66384a8
Jacob Morris <jacob.s.morris@gmail.com>
2011-08-13 21:45:39 -0600

    cleanup connection_spec so we aren't testing for methods we don't need yet

--- spec/connections/connection_spec.rb
+++ spec/connections/connection_spec.rb
@@ -7,10 +7,7 @@ module Arc
       it 'specifies methods that should be overwritten by specific adapters' do
         c = Connection.new
         c.should respond_to(:execute)
-        c.should respond_to(:active?)
-        
         lambda { c.execute "INSERT INTO superheros VALUES(superman)"}.should raise_error(NotImplementedError)
-        lambda { c.active? }.should raise_error(NotImplementedError)
       end
             
     end
