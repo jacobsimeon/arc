f53200813c96c59e7f5495df6940e7b541fd4363
Jacob Morris <jacob.s.morris@gmail.com>
2011-09-18 00:01:27 -0600

    simplify the data store api by using a schema object, remove arel dependency, don't include quoting since it isn't being used yet

--- lib/arc/data_stores/abstract_data_store.rb
+++ lib/arc/data_stores/abstract_data_store.rb
@@ -1,23 +1,16 @@
 module Arc
   module DataStores
     class AbstractDataStore < ResourcePool
-      include Quoting
-                        
-      def primary_key name
-        #get the primary key column object for the passed table name
-        raise NotImplementedError        
+
+      !!!!!!!!!!!!!!!!!!!!
+        #include Quoting
+        #include Casting
+      !!!!!!!!!!!!!!!!!!!!     
+      
+      def schema
+        @schema ||= Schemas[@config[:adapter]].new(self)
       end
-    
-      def table_exists? name
-        #true if the table exists in the database
-        raise NotImplementedError        
-      end
-    
-      def tables
-        #an array of tables in the data store
-        raise NotImplementedError
-      end
-    
+      
       def create query
         #add new data
         raise NotImplementedError
@@ -37,24 +30,16 @@ module Arc
         #destroy existing data
         raise NotImplementedError
       end
-      
-      def visitor
-        @visitor ||= begin
-          Arel::Visitors.for self
-        end
-      end
-      
+          
       private      
-      private :with_resource
       def execute query
-        #adapters should override this method to execute an arbitrary query against the database
+        #adapters should override this method to execute a query against the database
         raise NotImplementedError
       end
-      def create_connection
-        raise NotImplementedError
-      end
-
+      
       #better semantics for a class that deals with 'connections' instead of 'resources'
+      private :with_resource
+      def create_connection; raise NotImplementedError; end
       def create_resource; create_connection; end
       alias :connection :resource
       alias :with_connection :with_resource
