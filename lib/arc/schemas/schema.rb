29f6d5fff83ad7058f2d8e1a5ecb073d99c5316b
Jacob Morris <jacob.s.morris@gmail.com>
2011-09-22 00:04:28 -0600

    make it so schema.rb doesn't make your eyes bleed

--- lib/arc/schemas/schema.rb
+++ lib/arc/schemas/schema.rb
@@ -1,33 +1,35 @@
 require 'q/collector'
 module Arc
   module Schemas
-    class Schema# < SchemaObject
+
+    class Schema
       include Collector
       alias :table_names :keys
-
-      class Table# < SchemaObject
-        include Collector
-        alias :column_names :keys
-
-        class Column
-          attr_reader :name
-          attr_reader :allows_null
-          attr_reader :default
-          def initialize store, properties={}
-            @data_store = store
-            @name = properties.delete :name
-            @allows_null = properties.delete :allows_null
-            @default = properties.delete :default
-            @is_pk = properties.delete(:pk) || properties.delete(:primary_key)
-            @stype = properties.delete(:type)
-          end
-          def pk?; @is_pk; end
-          alias :primary_key? :pk?
-          def type
-            raise NotImplementedError
-          end
-        end
-      end      
     end
+    
+    class Schema::Table
+      include Collector
+      alias :column_names :keys
+    end
+    
+    class Schema::Table::Column
+      attr_reader :name
+      attr_reader :allows_null
+      attr_reader :default
+      def initialize store, properties={}
+        @data_store = store
+        @name = properties.delete :name
+        @allows_null = properties.delete :allows_null
+        @default = properties.delete :default
+        @is_pk = properties.delete(:pk) || properties.delete(:primary_key)
+        @stype = properties.delete(:type)
+      end
+      def pk?; @is_pk; end
+      alias :primary_key? :pk?
+      def type
+        raise NotImplementedError
+      end
+    end
+    
   end
 end
