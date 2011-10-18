f1df9c94699e5814a7d36ea729322958ef3a680b
Jacob Morris <jacob.s.morris@gmail.com>
2011-10-17 20:12:05 -0600

    test methods that should raise NotImplementedError

--- spec/data_stores/abstract_data_store_spec.rb
+++ spec/data_stores/abstract_data_store_spec.rb
@@ -12,9 +12,29 @@ module Arc
         it 'does not define connection creation' do
           store = AbstractDataStore.new ArcTest.config[:empty]
           ->{ store.send :with_connection }.should raise_error(NotImplementedError)
-        end        
+        end
       end
-            
+      describe 'abstract methods throw NotImplementedError' do
+        before :each do
+          @store = AbstractDataStore.new ArcTest.config[:empty]
+          @query = "omg"
+        end
+        it '#create raises not implemented error' do
+          ->{ @store.create @query }.should raise_error(NotImplementedError)
+        end
+        it '#read raises not implemented error' do
+          ->{ @store.read @query }.should raise_error(NotImplementedError)          
+        end
+        it '#update raises not implemented error' do
+          ->{ @store.update @query }.should raise_error(NotImplementedError)          
+        end
+        it '#destroy raises not implemented error' do
+          ->{ @store.destroy @query }.should raise_error(NotImplementedError)          
+        end
+        it '#execute raises not implemented error' do
+          ->{ @store.execute @rquery }.should raise_error(NotImplementedError)          
+        end
+      end
     end
   end
 end
