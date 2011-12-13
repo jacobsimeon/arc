119b21e8b46371b9144c2f8297a76a0cb9bbc29b
Jacob Morris <jacob.s.morris@gmail.com>
2011-12-12 22:30:54 -0700

    simplify requires

--- lib/arc.rb
+++ lib/arc.rb
@@ -9,8 +9,5 @@ require 'q/symbol'
 require 'q/dispatcher'
 
 require "arc/version"
-require 'arc/casting/casting.rb'
-require 'arc/casting/quoting.rb'
 require 'arc/data_stores'
-require 'arc/schemas'
 
