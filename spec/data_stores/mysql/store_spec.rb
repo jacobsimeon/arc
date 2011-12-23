8d42cc4d4a08071e1c27abb23d926daa79a9624e
Jacob Morris <jacob.s.morris@gmail.com>
2011-12-22 23:01:07 -0700

    test mysql table/column quoting

--- spec/data_stores/mysql/store_spec.rb
+++ spec/data_stores/mysql/store_spec.rb
@@ -2,6 +2,15 @@ require 'spec_helper'
 require 'arc/data_stores/mysql/store'
 
 describe Arc::DataStores::MysqlDataStore do
+  describe '#quote_table_name', :wip => true do
+    store = Arc::DataStores::MysqlDataStore.new ArcTest.config[:mysql]
+    store.quote_table_name('my-table').should == "`my-table`"
+  end
+  describe '#quote_column_name', :wip => true do
+    store = Arc::DataStores::MysqlDataStore.new ArcTest.config[:mysql]
+    store.quote_column_name('my-column').should == "`my-column`"    
+  end
+  
   describe "#schema" do
     it 'is an instance of Arc::DataStores::MysqlSchema' do
       store = Arc::DataStores::MysqlDataStore.new ArcTest.config[:mysql]
