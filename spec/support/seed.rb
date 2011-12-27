dd74bd0303390ed522ec574cb687e21e70b9e138
Jacob Morris <jacob.s.morris@gmail.com>
2011-12-27 14:31:06 -0700

    better seed data, with binary files and many to many relationships

--- spec/support/seed.rb
+++ spec/support/seed.rb
@@ -1,4 +1,3 @@
-
 tables = Hash.new do |hash, key|
   hash[key] = Arel::Table.new key
 end
@@ -9,19 +8,19 @@ data = {
     {
       :name => 'superman',
       :born_on => Time.now,
-      :photo => 'file',#File.read("#{file_root}/resources/superman.gif"),
+      :photo => File.read("#{file_root}/resources/superman.gif"),
       :created_at => Time.now
     },
     {
       :name => 'ironman',
       :born_on => Time.now,
-      :photo => 'file',#File.read("#{file_root}/resources/ironman.gif"),
+      :photo => File.read("#{file_root}/resources/ironman.gif"),
       :created_at => Time.now
     },
     {
       :name => 'megaman',
       :born_on => Time.now,
-      :photo => 'file',#File.read("#{file_root}/resources/megaman.jpg"),
+      :photo => File.read("#{file_root}/resources/megaman.jpg"),
       :created_at => Time.now
     }
   ],
@@ -42,16 +41,34 @@ data = {
   ]
 }
 
+ids = {:superheros => {}, :powers => {}}
+
 data.each_pair do |name, tuples|  
   table = tables[name]
   delete = Arel::DeleteManager.new Arel::Table.engine
   Arel::Table.engine.destroy delete.from(table).to_sql
+  
   tuples.each do |tuple|
     stmt = Arel::InsertManager.new Arel::Table.engine
     values = tuple.keys.map do |key|
       [table[key], tuple[key]]
     end
     stmt.insert(values)
-    Arel::Table.engine.create stmt.to_sql
+    record = Arel::Table.engine.create(stmt.to_sql)
+    ids[name][record[:name]] = record[:id]
   end
+end
+power_map = Arel::Table.new :superheros_powers
+
+power_sets = [
+  ['superman', 'flight'],
+  ['ironman', 'money']
+]
+power_sets.each do |set|
+  im = Arel::InsertManager.new Arel::Table.engine
+  im.insert([
+    [power_map[:superhero_id], ids[:superheros][set[0]]],
+    [power_map[:power_id], ids[:powers][set[1]]]
+  ])
+  Arel::Table.engine.create im.to_sql
 end
